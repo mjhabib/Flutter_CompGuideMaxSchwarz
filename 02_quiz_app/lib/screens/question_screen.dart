46256e69672c110c68e2bd8d4fc588e202e76311
MJ Habib <mj.habib4@gmail.com>
2024-07-02 20:00:46 +0330

    display the first question and its answers on the screen

--- 02_quiz_app/lib/screens/question_screen.dart
+++ 02_quiz_app/lib/screens/question_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/answer_button.dart';
+import '../data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -11,24 +12,34 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'question',
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
-          const SizedBox(height: 30),
-          AnswerButton('answer 1', () {}),
-          const SizedBox(height: 20),
-          AnswerButton('answer 2', () {}),
-          const SizedBox(height: 20),
-          AnswerButton('answer 3', () {}),
-          const SizedBox(height: 20),
-          AnswerButton('answer 4', () {}),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.question,
+              style: const TextStyle(color: Colors.white, fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            // Show answers dynamically using map and spread operation
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(answer, () {});
+            })
+
+            // Show answers manually
+            // AnswerButton(currentQuestion.answers[0], () {}),
+            // AnswerButton(currentQuestion.answers[1], () {}),
+            // AnswerButton(currentQuestion.answers[2], () {}),
+            // AnswerButton(currentQuestion.answers[3], () {}),
+          ],
+        ),
       ),
     );
   }
