3451248df166d2490784c8b51178f41b1b0baed8
MJ Habib <mj.habib4@gmail.com>
2024-07-02 18:47:28 +0330

    design question_screen ui

--- 02_quiz_app/lib/screens/question_screen.dart
+++ 02_quiz_app/lib/screens/question_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/answer_button.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -10,6 +11,25 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'question',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton('answer 1', () {}),
+          const SizedBox(height: 20),
+          AnswerButton('answer 2', () {}),
+          const SizedBox(height: 20),
+          AnswerButton('answer 3', () {}),
+          const SizedBox(height: 20),
+          AnswerButton('answer 4', () {}),
+        ],
+      ),
+    );
   }
 }
---
+++ 02_quiz_app/lib/widgets/answer_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  final String textAnswer;
+  final void Function() onTap;
+  const AnswerButton(this.textAnswer, this.onTap, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+          padding: const EdgeInsetsDirectional.symmetric(
+              vertical: 10, horizontal: 30),
+          backgroundColor: const Color.fromARGB(255, 40, 85, 42),
+          foregroundColor: Colors.white,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(40))),
+      child: Text(textAnswer),
+    );
+  }
+}
