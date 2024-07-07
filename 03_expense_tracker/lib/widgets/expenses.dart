05a4611326e35fb6a98355bf1f127425979df5c2
MJ Habib <mj.habib4@gmail.com>
2024-07-07 18:21:54 +0330

    add a SnackBar to notify user an expense was deleted and functionality to undo this action

--- 03_expense_tracker/lib/widgets/expenses.dart
+++ 03_expense_tracker/lib/widgets/expenses.dart
@@ -43,15 +43,39 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(
       () {
         _registeredExpenses.remove(expense);
       },
     );
+    ScaffoldMessenger.of(context).clearSnackBars();
+    // clear previews snackBars before showing a new one
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: const Text('Expense deleted.'),
+      duration: const Duration(seconds: 3),
+      action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          }),
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+          expenses: _registeredExpenses, onRemoveExpense: _removeExpense);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Tracker'),
@@ -63,8 +87,7 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('chart data'),
           Expanded(
-            child: ExpensesList(
-                expenses: _registeredExpenses, onRemoveExpense: _removeExpense),
+            child: mainContent,
           ),
           // using a ListView inside a column cause a problem, that's why we need the 'Expanded' widget here
         ],
