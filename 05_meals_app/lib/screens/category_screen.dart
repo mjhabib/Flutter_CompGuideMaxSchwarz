a796cb1f25d58fd8c5ec2015a950c020f13cca5e
MJ Habib <mj.habib4@gmail.com>
2024-07-09 20:03:39 +0330

    display some dummy data on screen

--- 05_meals_app/lib/screens/category_screen.dart
+++ 05_meals_app/lib/screens/category_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
 
@@ -10,18 +13,16 @@ class CategoryScreen extends StatelessWidget {
         title: const Text('Pick your category'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             childAspectRatio: 3 / 2,
             crossAxisSpacing: 20,
             mainAxisSpacing: 20),
-        children: const [
-          Text('1', style: TextStyle(color: Colors.white)),
-          Text('2', style: TextStyle(color: Colors.white)),
-          Text('3', style: TextStyle(color: Colors.white)),
-          Text('4', style: TextStyle(color: Colors.white)),
-          Text('5', style: TextStyle(color: Colors.white)),
-          Text('6', style: TextStyle(color: Colors.white)),
+        children: [
+          //   availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
         ],
       ),
     );
