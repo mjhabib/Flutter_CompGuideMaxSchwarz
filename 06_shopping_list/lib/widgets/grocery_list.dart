6933a1b688df733c03f17094efd670a6eab6efbf
MJ Habib <mj.habib4@gmail.com>
2024-07-17 16:57:56 +0330

    add new_item widget and navigate to it from grocery_list widget

--- 06_shopping_list/lib/widgets/grocery_list.dart
+++ 06_shopping_list/lib/widgets/grocery_list.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 
 import 'package:shopping_list/data/item_data.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryItem extends StatelessWidget {
+class GroceryItem extends StatefulWidget {
   const GroceryItem({super.key});
+
+  @override
+  State<GroceryItem> createState() => _GroceryItemState();
+}
+
+class _GroceryItemState extends State<GroceryItem> {
+  void _addItem() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const NewItem()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
       body: ListView.builder(
         itemCount: itemData.length,
