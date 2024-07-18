581975fc2f91a6b8503f8cdccfb1357060c87e8c
MJ Habib <mj.habib4@gmail.com>
2024-07-18 18:21:39 +0330

    add logics like validation, saving and resetting items in the new_item widget

--- 06_shopping_list/lib/widgets/new_item.dart
+++ 06_shopping_list/lib/widgets/new_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:shopping_list/data/category_data.dart';
+import 'package:shopping_list/models/category_model.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -10,6 +11,26 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  // this key will help us to validate all widgets inside our form
+  // this 'GlobalKey' makes sure even if "build method" executes again, the form keeps its internal state
+  // also we can use this GlobalKey to access to the form's state and do things like validation
+  final _formKey = GlobalKey<FormState>();
+  var _enteredName = '';
+  var _enteredQuantity = 1;
+  var _selectedCategory = categoryData[Categories.vegetables]!;
+
+  void _saveItem() {
+    // 'validate' here will trigger 'validator' functions defined in our form
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      // 'save' here will trigger 'onSave' functions defined in our form
+    }
+  }
+
+  void _resetItem() {
+    _formKey.currentState!.reset();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +40,7 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               // instead of TextField widget
@@ -26,7 +48,16 @@ class _NewItemState extends State<NewItem> {
                 maxLength: 50,
                 decoration: const InputDecoration(label: Text('Name')),
                 validator: (value) {
-                  return 'demo...';
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return 'Must be between 1 and 50 characters';
+                  }
+                  return null;
+                },
+                onSaved: (newValue) {
+                  _enteredName = newValue!;
                 },
               ),
               Row(
@@ -34,16 +65,28 @@ class _NewItemState extends State<NewItem> {
                 children: [
                   Expanded(
                     child: TextFormField(
-                      initialValue: '1',
+                      initialValue: _enteredQuantity.toString(),
+                      keyboardType: TextInputType.number,
                       decoration:
                           const InputDecoration(label: Text('Quantity')),
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Enter a valid, positive number.';
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        _enteredQuantity = int.parse(newValue!);
+                      },
                     ),
                   ),
-                  const SizedBox(
-                    width: 9,
-                  ),
+                  const SizedBox(width: 9),
                   Expanded(
                     child: DropdownButtonFormField(
+                      value: _selectedCategory,
                       items: [
                         // 'entries' to convert our map items to list items
                         for (final category in categoryData.entries)
@@ -62,7 +105,13 @@ class _NewItemState extends State<NewItem> {
                             ),
                           ),
                       ],
-                      onChanged: (value) {},
+                      // since we're using onChanged here we no longer need to use onSaved for this widget because we are re-assigning any changes to a variable
+                      onChanged: (newValue) {
+                        setState(() {
+                          // the setState here is required because we used the _selectedCategory as value in our widget therefore these two should be in-sync on any changes
+                          _selectedCategory = newValue!;
+                        });
+                      },
                     ),
                   )
                 ],
@@ -71,9 +120,9 @@ class _NewItemState extends State<NewItem> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  TextButton(onPressed: () {}, child: const Text('Reset')),
+                  TextButton(onPressed: _resetItem, child: const Text('Reset')),
                   ElevatedButton(
-                      onPressed: () {}, child: const Text('Add item'))
+                      onPressed: _saveItem, child: const Text('Add item'))
                 ],
               )
             ],
