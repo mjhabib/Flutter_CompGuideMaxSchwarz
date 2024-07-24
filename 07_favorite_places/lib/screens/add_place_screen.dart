dee9eeb65cb04cf9b57c517e308fc35743cca8f1
MJ Habib <mj.habib4@gmail.com>
2024-07-24 21:29:50 +0330

    add functionality to get device's location using Location package

--- 07_favorite_places/lib/screens/add_place_screen.dart
+++ 07_favorite_places/lib/screens/add_place_screen.dart
@@ -1,5 +1,6 @@
 import 'package:favorite_places/providers/user_places_notifier.dart';
 import 'package:favorite_places/widgets/image_input_widget.dart';
+import 'package:favorite_places/widgets/location_input_widget.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -60,6 +61,10 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 ImageInputWidget(onPickImage: (image) {
                   _selectedImage = image;
                 }),
+                const SizedBox(
+                  height: 10,
+                ),
+                const LocationInputWidget(),
                 const SizedBox(height: 16),
                 ElevatedButton.icon(
                   onPressed: _savePlace,
---
+++ 07_favorite_places/lib/widgets/location_input_widget.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class LocationInputWidget extends StatefulWidget {
+  const LocationInputWidget({super.key});
+
+  @override
+  State<LocationInputWidget> createState() => _LocationInputWidgetState();
+}
+
+class _LocationInputWidgetState extends State<LocationInputWidget> {
+  late Location _pickedLocation;
+  var _isGettingLocation = false;
+
+// Note: Did not work on Chrome browser but did work on Android
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    setState(() {
+      _isGettingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+
+    setState(() {
+      _isGettingLocation = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      'No location chosen',
+      textAlign: TextAlign.center,
+      style: Theme.of(context)
+          .textTheme
+          .bodyLarge!
+          .copyWith(color: Theme.of(context).colorScheme.onSurface),
+    );
+
+    if (_isGettingLocation) {
+      previewContent = const CircularProgressIndicator();
+    }
+
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(
+                width: 1,
+                color: Theme.of(context).colorScheme.primary.withOpacity(0.2)),
+          ),
+          child: previewContent,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              label: const Text('Get current location'),
+              icon: const Icon(Icons.location_on),
+              onPressed: _getCurrentLocation,
+            ),
+            TextButton.icon(
+              label: const Text('Select on map'),
+              icon: const Icon(Icons.map),
+              onPressed: () {},
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
