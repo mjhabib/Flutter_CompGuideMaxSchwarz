9057e3dc9724cff3df5553f387e487a3b9143827
MJ Habib <mj.habib4@gmail.com>
2024-07-28 21:11:06 +0330

    add signOut functionality to the app

--- 08_chat_app/lib/screens/chat_screen.dart
+++ 08_chat_app/lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -7,6 +8,15 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: const Icon(Icons.exit_to_app),
+            color: Theme.of(context).colorScheme.primary,
+          )
+        ],
         title: const Text('FlutterChat'),
       ),
       body: const Center(
