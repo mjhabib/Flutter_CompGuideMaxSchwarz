1ea8e06b3a409dcde9959fa261a02087007f9234
MJ Habib <mj.habib4@gmail.com>
2024-07-29 08:32:56 +0330

    pick an image from gallery as avatar using image_picker package (for web version only)

---
+++ 08_chat_app/lib/widgets/user_image_picker.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({super.key});
+
+  @override
+  State<UserImagePicker> createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  String? _pickedImageFile;
+
+  Future<void> _pickImage() async {
+    final pickedImage = await ImagePicker().pickImage(
+        source: ImageSource.gallery, maxWidth: 150, imageQuality: 50);
+
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
+      _pickedImageFile = pickedImage.path;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+          foregroundImage:
+              _pickedImageFile != null ? NetworkImage(_pickedImageFile!) : null,
+        ),
+        TextButton.icon(
+          onPressed: _pickImage,
+          label: Text(
+            'Add Image',
+            style: TextStyle(color: Theme.of(context).colorScheme.primary),
+          ),
+          icon: const Icon(Icons.image),
+        ),
+      ],
+    );
+  }
+}
