3788299720628817991203ec897a469f3a662715
MJ Habib <mj.habib4@gmail.com>
2024-07-29 19:34:25 +0330

    show stored chat messages on screen using StreamBuilder

--- 08_chat_app/lib/widgets/chat_messages.dart
+++ 08_chat_app/lib/widgets/chat_messages.dart
@@ -1,12 +1,46 @@
-import 'package:flutter/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('No messages found.'),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: false)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No messages found.'),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('Something went wrong!'),
+          );
+        }
+
+        final loadedMessages = snapshot.data!.docs;
+
+        return ListView.builder(
+          itemCount: loadedMessages.length,
+          itemBuilder: (context, index) {
+            return Text(
+              loadedMessages[index].data()['text'],
+            );
+          },
+        );
+      },
     );
   }
 }
